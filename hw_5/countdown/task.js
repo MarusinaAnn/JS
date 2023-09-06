0eeaabdbb87749c33e6a5cd27790ed530cbe8da9
Ann Marusina <an.marusina2010@yandex.ru>
2023-09-06 16:04:42 +0300

    clearInterval

--- hw_5/countdown/task.js
+++ hw_5/countdown/task.js
@@ -3,11 +3,10 @@ const p=document.getElementById('timer')
 const time = function() {
     if (Number(p.textContent) >= 1) {
         p.textContent = Number(p.textContent) - 1;
-    } else if (p.textContent = '0') {
-        alert('You win!');
     } else if (Number(p.textContent) <= 0) {
-        clearInterval(time);
+        alert('You win!')
+        clearInterval(timer);
     }
 }
 
-setInterval(time, 1000);
+let timer = setInterval(time, 1000);
